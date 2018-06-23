0f539b3039bb397f5b9716f2ea22dd1510041ec0
Aaron Kennedy <aarontkennedy@MacBook-Air.local>
2018-06-23 11:09:03 -0500

    initial file structure based off of my king burgers app

---
+++ app/controller/routing/apiRoutes.js
@@ -0,0 +1,152 @@
+module.exports = function (app) {
+
+    const orm = require("../../database/orm.js");
+    let cachedBurgerSuggestionsJSON = null;
+
+    app.get("/autosuggest/burgers", function (req, res) {
+        //console.log(req.query.query);
+        orm.searchBurgerName(req.query.query, function (error, results) {
+            if (error) {
+                console.log(error);
+                return res.sendStatus(500);
+            }
+
+            //console.log(results);
+            if (results.length) {
+                let suggestions = [];
+                for (let i = 0; i < results.length; i++) {
+                    suggestions.push({
+                        value: results[i].name,
+                        data: results[i].id,
+                        description: results[i].description
+                    });
+                }
+                return res.json({ suggestions: suggestions });
+            }
+            else {
+                // no valid burger names were returned
+                // we can suggest names...
+                if (cachedBurgerSuggestionsJSON) {
+                    //console.log("returning cached buger suggestions!");
+                    return res.json(cachedBurgerSuggestionsJSON);
+                }
+
+                orm.getBurgers(function (error, results) {
+                    if (error) {
+                        console.log(error);
+                        return res.sendStatus(500);
+                    }
+
+                    let suggestions = [];
+                    for (let i = 0; i < results.length; i++) {
+                        suggestions.push(results[i].name);
+                    }
+                    cachedBurgerSuggestionsJSON = { suggestions: suggestions };
+                    return res.json(cachedBurgerSuggestionsJSON);
+                }, 5);
+
+            }
+
+        });
+    });
+
+    app.post("/api/burgers", function (req, res) {
+        //console.log(req);
+        if (!req.body.name || !req.body.description) {
+            return res.sendStatus(400);
+        }
+        orm.addBurger(req.body.name,
+            req.body.description,
+            function (error, result) {
+                if (error) {
+                    console.log(error);
+                    return res.sendStatus(500);
+                }
+                //console.log(result);
+                return res.json({
+                    id: result.insertId,
+                    name: req.body.name,
+                    description: req.body.description
+                });
+            });
+    });
+
+    app.get("/api/count", function (req, res) {
+        //console.log(req);
+
+        orm.getRowCountAllTables(function (error, result) {
+                if (error) {
+                    console.log(error);
+                    return res.sendStatus(500);
+                }
+                //console.log(result);
+/** 
+ *   results come back like this - not helpful
+ *  [ RowDataPacket { TABLE_NAME: 'burgers', TABLE_ROWS: 4 },
+ *    RowDataPacket { TABLE_NAME: 'burgersEaten', TABLE_ROWS: 6 },
+ *    RowDataPacket { TABLE_NAME: 'eaters', TABLE_ROWS: 1 } ]
+ * 
+ *      I change the array to an object like this:
+ *    {burgers: 4,
+ *     burgersEaten: 6,
+ *     eater: 1
+ *    }
+ */
+                function mapDataObject(arr) {
+                    let rv = {};
+                    for (let i = 0; i < arr.length; ++i)
+                      // go through each element of the array and 
+                      // give the object a key with table_name and its value
+                      // table_rows.
+                      rv[arr[i].TABLE_NAME] = arr[i].TABLE_ROWS;
+                    return rv;
+                  }
+                  // pretty flukey case - when the app is run for the first time with
+                  // no data - I return null...  It is checked for on the client side.
+                let prettierMappedData = (result.length ? mapDataObject(result) : null);
+                return res.json(prettierMappedData);
+            });
+    });
+
+    // creates a record of a particular burger eaten by a particular person
+    app.post("/api/burgerseaten", function (req, res) {
+        //console.log(req.body);
+        if (!req.body.burger_id || !req.body.eater_id) {
+            return res.sendStatus(400);
+        }
+
+        orm.addBurgerEaten(req.body.eater_id,
+            req.body.burger_id,
+            req.body.rating,
+            function (error, result) {
+                if (error) {
+                    //console.log(error);
+                    return res.sendStatus(500);
+                }
+                //console.log(result);
+                return res.json({
+                    id: result.insertID,
+                    eater_id: req.body.eater_id,
+                    burger_id: req.body.burger_id,
+                    rating: req.body.rating
+                });
+            });
+    });
+
+    app.get("/api/burgerseaten/:eater_id", function (req, res) {
+        if (!req.params.eater_id) {
+            return res.sendStatus(400);
+        }
+
+        orm.getBurgersEaten(req.params.eater_id,
+            function (error, result) {
+                if (error) {
+                    console.log(error);
+                    return res.sendStatus(500);
+                }
+                //console.log(result);
+                return res.json(result);
+            });
+    });
+
+};
---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies
+// =========================================================
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =========================================================
+let app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ layoutsDir: "app/public/views/layouts", 
+                                  defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.set('views', 'app/public/views');
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
+
+// express will handle the static files
+app.use(express.static('app/public'));
+
+// Get the routes
+require('./app/controller/routing/staticRoutes.js')(app);
+require('./app/controller/routing/dynamicRoutes.js')(app);
+require('./app/controller/routing/oauthRoutes.js')(app);
+
+// Starts the server to begin listening
+// =========================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
