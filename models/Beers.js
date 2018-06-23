c12da352963d7b534428f896d5b1deabca30f18f
Aaron Kennedy <aarontkennedy@MacBook-Air.local>
2018-06-23 12:38:51 -0500

    created the seed file for beers

--- models/Beers.js
+++ models/Beers.js
@@ -2,32 +2,24 @@ module.exports = function (sequelize, DataTypes) {
 
     let Beers = sequelize.define("Beers", {
         name: {
-            type: DataTypes.STRING
-            //allowNull: false - google sometimes doesn't give names
+            type: DataTypes.STRING,
+            allowNull: false
         }, 
         brewery: {
-            type: DataTypes.STRING
-            //allowNull: false - google sometimes doesn't give names
+            type: DataTypes.STRING,
+            allowNull: false
         },  
-        category: {
-            type: DataTypes.STRING
-            //allowNull: false - google sometimes doesn't give names
-        }, 
         style: {
             type: DataTypes.STRING
-            //allowNull: false - google sometimes doesn't give names
         }, 
         abv: {
             type: DataTypes.FLOAT
-            //allowNull: false - google sometimes doesn't give names
         }, 
         ibu: {
             type: DataTypes.FLOAT
-            //allowNull: false - google sometimes doesn't give names
         },  
-        style: {
+        description: {
             type: DataTypes.TEXT
-            //allowNull: false - google sometimes doesn't give names
         }
     });
 
